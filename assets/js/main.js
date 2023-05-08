931f1780941ee610bedd69bc98aae153da6e1804
Sazib Banik <sazib.banik.1@gmail.com>
2023-05-08 11:50:35 +0600

    success-part done

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,3 +15,20 @@ var swiper = new Swiper(".mySwiper", {
         prevEl: ".swiper-button-prev",
       },
 });
+var swiper = new Swiper(".mySwiperOne", {
+    spaceBetween: 30,
+      centeredSlides: true,
+      autoplay: {
+        delay: 2000,
+        disableOnInteraction: false,
+      },
+    
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+});
