b4524b3b39ea0e1e9a731691ad6dfee1c62d74f9
Sazib Banik <sazib.banik.1@gmail.com>
2023-05-14 15:53:46 +0600

    hamid vai er page add korlam

--- assets/js/main.js
+++ assets/js/main.js
@@ -41,3 +41,23 @@ $('.counter').counterUp({
   delay: 10,
   time: 2500
 });
+//owl-carousel active
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: true,
+  autoplay: true,
+  autoplayTimeout: 1000,
+  autoplayHoverPause: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 3,
+    },
+    1000: {
+      items: 5,
+    },
+  },
+});
