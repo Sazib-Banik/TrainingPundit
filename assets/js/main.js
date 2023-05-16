b527bb8648c4fd7e4e7a5271ffae86cd088b0454
Sazib Banik <sazib.banik.1@gmail.com>
2023-05-16 17:16:53 +0600

    swiper js convert into owl-carousel

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,52 +1,103 @@
 var swiper = new Swiper(".mySwiper", {
-    spaceBetween: 30,
-      centeredSlides: true,
-      autoplay: {
-        delay: 3500,
-        disableOnInteraction: false,
-      },
-    
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-      },
-      navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      },
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 3500,
+    disableOnInteraction: false,
+  },
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
 });
+// success story carousel
 var swiper = new Swiper(".mySwiperOne", {
-    spaceBetween: 30,
-      centeredSlides: true,
-      autoplay: {
-        delay: 2000,
-        disableOnInteraction: false,
-      },
-    
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-      },
-      navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      },
+  effect: "coverflow",
+  centeredSlides: true,
+  autoplay: {
+    delay: 3500,
+    disableOnInteraction: false,
+  },
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+});
+// top-categorie
+var swiper = new Swiper(".top-category", {
+  slidesPerView: 4,
+  spaceBetween: 20,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+// feature-categories
+var swiper = new Swiper(".feature-categories", {
+  slidesPerView: 4,
+  spaceBetween: 20,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
 });
 // venobox active
 new VenoBox({
-  selector: '.venobox'
+  selector: ".venobox",
 });
 // counter js active
-$('.counter').counterUp({
+$(".counter").counterUp({
   delay: 10,
-  time: 2500
+  time: 2500,
 });
 //owl-carousel active
-$(".owl-carousel").owlCarousel({
-  loop: true,
-  margin: 10,
+// $(".owl-carousel").owlCarousel({
+//   loop: true,
+//   margin: 10,
+//   nav: true,
+//   autoplay: true,
+//   autoplayTimeout: 1000,
+//   autoplayHoverPause: true,
+//   responsive: {
+//     0: {
+//       items: 1,
+//     },
+//     600: {
+//       items: 3,
+//     },
+//     1000: {
+//       items: 5,
+//     },
+//   },
+// });
+$(".your-goal").owlCarousel({
+  
+  margin: 7,
+  dots:false,
   nav: true,
-  autoplay: true,
+  autoplay: false,
   autoplayTimeout: 1000,
   autoplayHoverPause: true,
   responsive: {
@@ -57,7 +108,28 @@ $(".owl-carousel").owlCarousel({
       items: 3,
     },
     1000: {
-      items: 5,
+      items: 4,
+    },
+  },
+});
+
+$(".top-category-carousel").owlCarousel({
+  loop:true,
+  margin: 7,
+  dots:false,
+  nav: true,
+  autoplay: false,
+  autoplayTimeout: 1000,
+  autoplayHoverPause: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 3,
+    },
+    1000: {
+      items: 4,
     },
   },
 });
